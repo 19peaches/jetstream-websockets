59e95ec24dcc433432dfdb16e276c5f5ae6e2b43
Vince Kronlein <vkronlein@icloud.com>
2021-02-15 01:53:48 -0700

    websocket dashboard access

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,6 +26,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return in_array($user->email, [
+                'vince@19peaches.com',
+            ]);
+        });
     }
 }
